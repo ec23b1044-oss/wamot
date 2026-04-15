aa164fcaa18f9e4aecbcd51f0679e4c9aa62f8b3
ec23b1044-oss <ec23b1044@iiitdm.ac.in>
2026-04-15 15:39:12 +0530

    Create _auth.js

---
+++ api/_auth.js
@@ -0,0 +1,9 @@
+export function verifyToken(req) {
+  const auth = req.headers.authorization;
+
+  if (!auth) return null;
+
+  const token = auth.split(" ")[1];
+
+  return sessions[token] || null;
+}
